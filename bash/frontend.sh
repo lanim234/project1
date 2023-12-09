dddb58e895294f4f3271820d0c3816f7feff6097
lanim <niyi_meg@yahoo.com>
2023-12-09 12:11:46 -0600

    frontend configuration

--- bash/frontend.sh
+++ bash/frontend.sh
@@ -4,20 +4,20 @@ dnf install nginx -y
 echo -e "\e[31m Removing html content in nginx \e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[31m Removing html content in nginx \e[0m"
+echo -e "\e[33m Removing html content in nginx \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[31m changing to nginx\html folder \e[0m"
+echo -e "\e[33m changing to nginx\html folder \e[0m"
 cd /usr/share/nginx/html
 
-echo -e "\e[31m extracting frontend content \e[0m"
+echo -e "\e[33m extracting frontend content \e[0m"
 unzip /tmp/frontend.zip
 
-echo -e "\e[31m copying roboshop.conf file \e[0m"
-cp /Users/adeniyimegbabi/project1 /etc/nginx/default.d/roboshop.conf
+echo -e "\e[33m copying roboshop.conf file \e[0m"
+cp /Users/adeniyimegbabi/project1/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[31m enable nginx \e[0m"
+echo -e "\e[33m enable nginx \e[0m"
 systemctl enable nginx
 
-echo -e "\e[31m restart nginx \e[0m"
+echo -e "\e[33m restart nginx \e[0m"
 systemctl restart nginx
