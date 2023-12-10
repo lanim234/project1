28f78fd5840f05e57faa54bb034527529482b292
lanim <niyi_meg@yahoo.com>
2023-12-10 07:10:20 -0600

    mysql configuration

--- bash/mysql.sh
+++ bash/mysql.sh
@@ -2,15 +2,15 @@ echo -e "\e[32m Module Disable mysql\e[0m"
 dnf module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[32 Copy mysql repo file \e[0m"
-cp /root/project1/bash/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+cp /home/centos/project1/bash/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[32m Install mysql \e[0m"
 dnf install mysql-community-server -y &>>/tmp/roboshop.log
 
-echo -e "\e[32m Enable mysql \e[0m"
+echo -e "\e[32m Enable mysqld \e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 
-echo -e "\e[32m Restart mysql \e[0m"
+echo -e "\e[32m Restart mysqld \e[0m"
 systemctl restart mysqld &>>/tmp/roboshop.log
 
 echo -e "\e[32m Mysql password \e[0m"
