a3b86885014c2db291872358da0ae108d46c3a34
lanim <niyi_meg@yahoo.com>
2023-12-09 21:59:26 -0600

    cart configuration

--- bash/cart.sh
+++ bash/cart.sh
@@ -14,13 +14,13 @@ echo -e "\e[35m Make App Directory \e[0m"
 mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[35m Download User  Personalised artifacts \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
 
 echo -e "\e[35m Change to App Directory \e[0m"
 cd /app
 
-echo -e "\e[35m Extract downloaded file; user \e[0m"
-unzip /tmp/user.zip &>>/tmp/roboshop.log
+echo -e "\e[35m Extract downloaded file;  \e[0m"
+unzip /tmp/cart.zip &>>/tmp/roboshop.log
 
 echo -e "\e[35m Change to App Directory\e[0m"
 cd /app
@@ -28,17 +28,17 @@ cd /app
 echo -e "\e[35m Install npm \e[0m"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[35m Copy User.service \e[0m"
-cp root/project1/bash/user.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+echo -e "\e[35m Copy cart.service \e[0m"
+cp root/project1/bash/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
 
 echo -e  "\e[31m Reload Daemon\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 
-echo -e "\e[35m Enable user \e[0m"
-systemctl enable user &>>/tmp/roboshop.log
+echo -e "\e[35m Enable cart \e[0m"
+systemctl enable cart &>>/tmp/roboshop.log
 
-echo -e "\e[35m Restart User \e[0m"
-systemctl restart user &>>/tmp/roboshop.log
+echo -e "\e[35m Restart cart \e[0m"
+systemctl restart cart &>>/tmp/roboshop.log
 
 
 
