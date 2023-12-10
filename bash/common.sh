c6701ee53ec98e627a48e5ee638de50f6367b59b
lanim <niyi_meg@yahoo.com>
2023-12-10 13:17:39 -0600

    variable

--- bash/common.sh
+++ bash/common.sh
@@ -3,6 +3,43 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup() {
+  echo -e "${color} Add user ${nocolor}"
+  useradd roboshop &>>${log_file}
+
+  echo -e "${color} Make Directory ${nocolor}"
+  mkdir ${app_path} &>>${log_file}
+
+
+  echo -e "${color} Download  Content ${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+
+  echo -e "${color} Change to App Directory ${nocolor}"
+  cd ${app_path}
+
+  echo -e "${color} Extract  content ${nocolor}"
+  unzip /tmp/${component}.zip &>>${log_file}
+
+  echo -e "${color} Change to App directory ${nocolor}"
+  cd ${app_path}
+
+}
+
+
+systemD_setup () {
+  echo -e "${color} extracting frontend content ${nocolor}"
+  cp /home/centos/project1/bash/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  echo -e "${color} Reload Daemon ${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+
+  echo -e "${color} Enable ${component} ${nocolor}"
+  systemctl enable ${component} &>>${log_file}
+
+  echo -e "${color} Start ${component} ${nocolor}"
+  systemctl start ${component} &>>${log_file}
+}
+
 
 nodejs () {
   echo -e "${color} Disable NodeJs ${nocolor}"
@@ -14,50 +51,49 @@ nodejs () {
   echo -e "${color} Install NodeJs ${nocolor}"
   dnf install nodejs -y &>>${log_file}
 
-  echo -e "${color} Add user ${nocolor}"
-  useradd roboshop &>>${log_file}
-
-  echo -e "${color} Make Directory ${nocolor}"
-  mkdir ${app_path} &>>${log_file}
-
-
-  echo -e "${color} Download  Content ${nocolor}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
-
-  echo -e "${color} Change to App Directory ${nocolor}"
-  cd ${app_path}
-
-  echo -e "${color} Extract  content ${nocolor}"
-  unzip /tmp/$component.zip &>>${log_file}
-
-  echo -e "${color} Change to App Directory ${nocolor}"
-  cd ${app_path}
+  app_presetup
 
   echo -e "${color} Install Npm ${nocolor}"
   npm install &>>${log_file}
 
-  echo -e "${color} extracting frontend content ${nocolor}"
-  cp /home/centos/project1/bash/$component.service /etc/systemd/system/$component.service &>>${log_file}
-  #
-  echo -e "${color} Reload Daemon ${nocolor}"
-  systemctl daemon-reload &>>${log_file}
-
-  echo -e "${color} Enable $component ${nocolor}"
-  systemctl enable $component &>>${log_file}
-
-  echo -e "${color} Start $component ${nocolor}"
-  systemctl start $component &>>${log_file}
+  systemD_setup
 }
 
+
 mongo_schema_setup () {
-  echo -e "\e[35m Copy Mongo.repo \e[0m"
-  cp /home/centos/project1/bash/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+  echo -e "${color} Copy Mongo.repo ${nocolor}"
+  cp /home/centos/project1/bash/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-  echo -e "\e[35m Install Mongodb \e[0m"
-  dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+  echo -e "${color} Install Mongodb ${nocolor}"
+  dnf install mongodb-org-shell -y &>>${log_file}
 
-  echo -e "\e[35m Load Schema \e[0m"
-  mongo --host mongodb-dev.lanim.store </app/schema/user.js &>>/tmp/roboshop.log
+  echo -e "${color} Load Schema ${nocolor}"
+  mongo --host mongodb-dev.lanim.store </app/schema/user.js &>>${log_file}
 
+}
+
+mysql_schema_setup () {
+  echo -e "${color} Install mysql ${nocolor}"
+  dnf install mysql -y &>>${log_file}
+
+  echo -e "${color} Set mysql password ${nocolor}"
+  mysql -h mysql-dev.lanim.store -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql &>>${log_file}
+}
+
+maven () {
+  echo -e "${color} Install Maven ${nocolor}"
+  dnf install maven -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color} Maven package ${nocolor}"
+  mvn clean package &>>${log_file}
+
+
+  echo -e "${color} Move file to shippimng.jar ${nocolor}"
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+
+  systemD_setup
 
 }
