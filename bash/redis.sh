0cfb5336982f7d63f67fb0e737ae3dd688c6b1d8
lanim <niyi_meg@yahoo.com>
2023-12-10 06:39:36 -0600

    redis configuration

--- bash/redis.sh
+++ bash/redis.sh
@@ -7,7 +7,7 @@ dnf module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[34m Installing redis \e[0m"
 dnf install redis -y &>>/tmp/roboshop.log
 
-echo -e "\e[34m Reverse Proxy \e[0m"
+echo -e "\e[34m Update Listening Address \e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>/tmp/roboshop.log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>/tmp/roboshop.log
 
