cb06abf3c4776afbb63086d6deeae93e4329e609
lanim <niyi_meg@yahoo.com>
2023-12-10 11:15:08 -0600

    dispatch configuration

--- bash/dispatch.sh
+++ bash/dispatch.sh
@@ -1,53 +1,57 @@
-echo -e "\e[35m Install Golang \e[0m"
-dnf install golang -y &>>/tmp/roboshop.log
-
-echo -e "\e[35m User add Roboshop \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[35m Make App Directory \e[0m"
-mkdir /app &>>/tmp/roboshop.log
+source common.sh
+component=dispatch
 
 
-echo -e "\e[35m Download dispatch artifact \e[0m"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/roboshop.log
+echo -e "${noclor} Install Golang ${noclor}"
+dnf install golang -y &>>${log_file}
+
+echo -e "${noclor} User add Roboshop ${noclor}"
+useradd roboshop &>>${log_file}
+
+echo -e "${noclor} Make App Directory ${noclor}"
+mkdir ${app_path} &>>${log_file}
 
 
-echo -e "\e[35m Change to App Directory \e[0m"
-cd /app
+echo -e "${noclor} Download $component artifact ${noclor}"
+curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
 
 
-echo -e "\e[35m Extract dispatch artifact \e[0m"
-unzip /tmp/dispatch.zip &>>/tmp/roboshop.log
+echo -e "${noclor} Change to App Directory ${noclor}"
+cd ${app_path}
 
 
-echo -e "\e[35m Change to App Directory \e[0m"
-cd /app
+echo -e "${noclor} Extract $component artifact ${noclor}"
+unzip /tmp/$component.zip &>>${log_file}
 
 
-echo -e "\e[35m Go Mod \e[0m"
-go mod init dispatch &>>/tmp/roboshop.log
+echo -e "${noclor} Change to App Directory ${noclor}"
+cd ${app_path}
+
+
+echo -e "${noclor} Go Mod ${noclor}"
+go mod init $component &>>${log_file}
 
 
 
-echo -e "\e[35m Go get \e[0m"
-go get &>>/tmp/roboshop.log
+echo -e "${noclor} Go get ${noclor}"
+go get &>>${log_file}
 
 
-echo -e "\e[35m Go Build \e[0m"
-go build &>>/tmp/roboshop.log
+echo -e "${noclor} Go Build ${noclor}"
+go build &>>${log_file}
 
 
-echo -e "\e[35m Copy dispatch.service file  \e[0m"
-cp /home/centos/project1/bash/dispatch.service /etc/systemd/system/dispatch.service &>>/tmp/roboshop.log
+echo -e "${noclor} Copy $component.service file  ${noclor}"
+cp /home/centos/project1/bash/$component.service /etc/systemd/system/$component.service &>>${log_file}
 
 
-echo -e "\e[35m Reload Daemon \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
+echo -e "${noclor} Reload Daemon ${noclor}"
+systemctl daemon-reload &>>${log_file}
 
 
-echo -e "\e[35m Enable dispatch \e[0m"
-systemctl enable dispatch &>>/tmp/roboshop.log
+echo -e "${noclor} Enable $component ${noclor}"
+systemctl enable $component &>>${log_file}
 
 
-echo -e "\e[35m Restart dispatch \e[0m"
-systemctl restart dispatch &>>/tmp/roboshop.log
+echo -e "${noclor} Restart $component ${noclor}"
+systemctl restart $component &>>${log_file}
