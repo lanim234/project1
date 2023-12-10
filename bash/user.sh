4114eee8e4142153e5fb16c4a738c74f0487c943
lanim <niyi_meg@yahoo.com>
2023-12-10 11:43:07 -0600

    user configuration

--- bash/user.sh
+++ bash/user.sh
@@ -1,52 +1,6 @@
-echo -e "\e[35m Module Disable Nodejs \e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
+source common.sh
+component=user
 
-echo -e "\e[35m Module enable Nodejs \e[0m"
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+nodejs
 
-echo -e "\e[35m Install Nodejs \e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
-
-echo -e "\e[35m Add user; Roboshop \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[35m Make App Directory \e[0m"
-mkdir /app &>>/tmp/roboshop.log
-
-echo -e "\e[35m Download User  Personalised artifacts \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
-
-echo -e "\e[35m Change to App Directory \e[0m"
-cd /app
-
-echo -e "\e[35m Extract downloaded file; user \e[0m"
-unzip /tmp/user.zip &>>/tmp/roboshop.log
-
-echo -e "\e[35m Change to App Directory\e[0m"
-cd /app
-
-echo -e "\e[35m Install npm \e[0m"
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[35m Copy User.service \e[0m"
-cp /home/centos/project1/bash/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
-
-echo -e  "\e[31m Reload Daemon\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-
-echo -e "\e[35m Enable user \e[0m"
-systemctl enable user &>>/tmp/roboshop.log
-
-echo -e "\e[35m Restart User \e[0m"
-systemctl restart user &>>/tmp/roboshop.log
-
-echo -e "\e[35m Copy Mongo.repo \e[0m"
-cp /home/centos/project1/bash/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-
-echo -e "\e[35m Install Mongodb \e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
-
-echo -e "\e[35m Load Schema \e[0m"
-mongo --host mongodb-dev.lanim.store </app/schema/user.js &>>/tmp/roboshop.log
-
-
+mongo_schema_setup
