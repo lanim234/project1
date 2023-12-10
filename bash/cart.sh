e0085d39917af0a5cfd430df3db7dff72096ec20
lanim <niyi_meg@yahoo.com>
2023-12-10 06:57:02 -0600

    cart configuration

--- bash/cart.sh
+++ bash/cart.sh
@@ -13,22 +13,28 @@ useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[35m Make App Directory \e[0m"
 mkdir /app &>>/tmp/roboshop.log
 
-echo -e "\e[35m Change to App Directory \e[0m"
-cd /app
 
 echo -e "\e[35m Download cart  Personalised artifacts \e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
 
 
+echo -e "\e[35m Change to App Directory \e[0m"
+cd /app
+
+
 echo -e "\e[35m Extract downloaded file;  \e[0m"
 unzip /tmp/cart.zip &>>/tmp/roboshop.log
+
+echo -e "\e[35m Change to App Directory \e[0m"
+cd /app
+
 
 
 echo -e "\e[35m Install npm \e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[35m Copy cart.service \e[0m"
-cp root/project1/bash/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+cp /home/centos/project1/bash/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
 
 echo -e  "\e[31m Reload Daemon\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
