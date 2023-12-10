bb174034aae79962e822f0f1c18156e5758de791
lanim <niyi_meg@yahoo.com>
2023-12-10 06:22:55 -0600

    mongodb configuration

--- bash/mongodb.sh
+++ bash/mongodb.sh
@@ -7,8 +7,8 @@ dnf install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[33m Update mongoDB Listen Address \e[0m"
 sed -i  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
-echo -e "\e[33m Copy mongo repo file\e[0m"
+echo -e "\e[33m Enable Mongodb \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 
-echo -e "\e[33m Copy mongo repo file\e[0m"
+echo -e "\e[33m Restart MongoDb \e[0m"
 systemctl restart mongod &>>/tmp/roboshop.log
