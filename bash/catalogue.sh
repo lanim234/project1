783a72f1d65930fb87a958badfc0ebc878cb17aa
lanim <niyi_meg@yahoo.com>
2023-12-10 11:42:10 -0600

    catalogue configuration

--- bash/catalogue.sh
+++ bash/catalogue.sh
@@ -3,12 +3,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} Copy MongoDB Repo file ${nocolor}"
-cp /home/centos/project1/bash/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-
-echo -e "${color} Install MongoDB ${nocolor}"
-dnf install mongodb-org-shell -y /&>>${log_file}
-
-echo -e "${color} Load Schema ${nocolor}"
-mongo --host mongodb-dev.lanim.store <${app_path}/schema/$component.js &>>${log_file}
-
+mongo_schema_setup
